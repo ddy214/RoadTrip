0092d83c5f63bd56bb3d1f164fc6a466c3136880
Derek Yu <derekyu@Dereks-MacBook-Pro.local>
2016-11-20 16:31:55 -0500

    project structure

---
+++ app/src/main/java/mbd/org/roadtrip/Views/Activities/BaseActivity.java
@@ -0,0 +1,8 @@
+package mbd.org.roadtrip.Views.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+}
